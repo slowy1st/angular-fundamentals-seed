265f0d588cb35f8119a92d2f54532a5ef6190bb9
slowy <slowfly1st@gmail.com>
2020-04-15 09:32:18 +0200

    adds first component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
+    <div>
+      {{ title }}
     </div>
   `
 })
-export class AppComponent {
 
+export class AppComponent {
+  title: string;
+
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 }
