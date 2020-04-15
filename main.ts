38479829f9e9ec34e136b08ee86e36fb925f7630
slowy <slowfly1st@gmail.com>
2020-04-15 10:22:10 +0200

    bootstrapping the app module

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// there's different ways to bootstrap an app: on the server, on the client, or precompile it
+// -> platformBrowserDynamic runs on the client
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
+import {AppModule} from "./app/app.module";
 
 platformBrowserDynamic().bootstrapModule(AppModule);
