ffa2778ea783739a6a5145ade5094b52b5686b5e
slowy <slowfly1st@gmail.com>
2020-04-15 10:48:48 +0200

    one way property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,31 +3,21 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
+  // [innerHTML]'s -> tell angular that you want to dynamically bind to the actual source property
+  // [] bind directly to the element
+  // this is one-way binding only!
   template: `
     <div class='app'>
-      {{ title + '!'}}
-      <div>
-        <div>
-        {{ numberOne }}
-        {{ numberTwo }}
-        </div>
-
-        <div>
-        {{ numberOne + numberOne }}
-        </div>
-        <div>
-          {{ isHappy ? ':-)' : ':('}}
-        </div>
-      </div>
+      <h1 [innerHTML]="title"></h1>
+      <input type="text" [value]="name"/>
+      <div>{{ name }}</div>
     </div>
   `
 })
 
 export class AppComponent {
   title: string;
-  numberOne: number = 1;
-  numberTwo: number = 2;
-  isHappy: boolean = true;
+  name: string = 'slowy';
 
   constructor() {
     this.title = 'Ultimate Angular';
